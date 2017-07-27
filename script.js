898bf626a049fc2cb5267616e11310bbf9f2c704
Ami Schroeder <arschroeder22@gmail.com>
2017-07-27 10:40:21 -0500

    complete css effect

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('button').click(function () {
+        $('.cool-gradient').fadeToggle();
+        $('.awesome-gradient').fadeToggle();
+        $('.gnarly-gradient').fadeToggle();
+    });
+});
